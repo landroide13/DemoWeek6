7a8666a4f8f7c6512cfc377610206d945c6f847b
landroide13 <landrade1313@gmail.com>
2022-04-05 10:19:47 +1200

    Order Form

---
+++ DemoWeek6/OrderForm.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DemoWeek6
+{
+    public partial class OrderForm : ContentPage
+    {
+        public OrderForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
